a20c304ecbfcc391ef3057d7f8644d1b1c78dea7
uday <uday@DESKTOP-H9J11KV>
2021-05-30 01:31:25 +0530

    test demo class

---
+++ HelloWorld/src/DemoDemo.java
@@ -0,0 +1,22 @@
+
+public class DemoDemo {
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println(5);
+		System.out.println("5");
+		System.out.println(5+5);
+		System.out.println(5+"5");
+
+		System.out.println("5"+"5"+"5"+"5");
+		 int a=5;
+		 System.out.println(a);
+		 System.out.println(a+a);
+		
+
+	}
+
+
+
+}
